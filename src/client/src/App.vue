4e42191c5e6ade54eab2491b53637cb579dacee6
Nicola Zirilli <nicola.zirilli@fanplayr.com>
2019-03-14 22:50:08 +0100

    include vuetify

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,20 +1,36 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <Toolbar v-on:toggle-nav="toggleNav" />
+    <v-content>
+      <v-navigation-drawer v-model="drawer" app clipped>
+        <Treeview />
+      </v-navigation-drawer>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script>
+import HelloWorld from "./components/HelloWorld";
+import Toolbar from "./layout/Toolbar.vue";
+import Treeview from "./layout/Treeview.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Toolbar,
+    Treeview
+  },
+  data() {
+    return {
+      drawer: null
+    };
+  },
+  methods: {
+    toggleNav() {
+      this.drawer = !this.drawer;
+    }
+  }
+};
+</script>
---
+++ src/client/src/layout/Toolbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-toolbar app fixed clipped-left>
+    <v-toolbar-side-icon class="hidden-lg-and-up" @click="$emit('toggle-nav')"></v-toolbar-side-icon>
+    <v-toolbar-title class="headline text-uppercase">
+      <span>{{ title }}</span>
+      <span class="font-weight-light"></span>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+      <span class="mr-2">Latest Release</span>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: "Toolbar",
+  props: {
+    title: String
+  }
+};
+</script>
+
