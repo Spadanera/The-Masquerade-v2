3f9f367116b077821cb1029945d4b74bea057ac3
Nicola Zirilli <nicola.zirilli@fanplayr.com>
2019-04-05 15:23:13 +0200

    set docker db

--- src/server/app.js
+++ src/server/app.js
@@ -5,8 +5,8 @@ import mongoose from "mongoose";
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = require('bluebird');
-// mongoose.connect('mongodb://172.17.0.2:27017/the-masquerade', { promiseLibrary: require('bluebird'), useNewUrlParser: true })
-mongoose.connect('mongodb+srv://admin:admin@themasquerade-dfbrl.mongodb.net/test?retryWrites=true', { promiseLibrary: require('bluebird'), useNewUrlParser: true })
+mongoose.connect('mongodb://172.17.0.2:27017/the-masquerade', { promiseLibrary: require('bluebird'), useNewUrlParser: true })
+// mongoose.connect('mongodb+srv://admin:admin@themasquerade-dfbrl.mongodb.net/test?retryWrites=true', { promiseLibrary: require('bluebird'), useNewUrlParser: true })
   .then(() => console.log('connection succesful'))
   .catch((err) => console.error(err));
 
