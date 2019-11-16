b38df9166768e33f2a0bbe1fcebd398eb8ee171d
Nicola Zirilli <nicola.zirilli@fanplayr.com>
2019-11-16 16:26:17 +0100

    login card

--- client/src/views/public/Home.vue
+++ client/src/views/public/Home.vue
@@ -1,13 +1,29 @@
 <template>
   <v-layout wrap justify-space-around v-show="loaded">
-    <v-flex xs12 sm8 md6 lg4 x3>
+    <v-flex xs12 sm6 md6 lg5 xl3 pa-3>
       <v-card>
-        <v-btn @click="login('story-teller')">Login as Story Teller</v-btn>
+        <v-img :src="require('@/assets/story-teller-card.png')" height="300px"></v-img>
+        <v-card-title>Story Teller</v-card-title>
+        <v-card-subtitle>Manage yuor chronicle an invite players</v-card-subtitle>
+        <v-card-actions>
+          <v-btn @click="login('story-teller')" color="primary">
+            <img class="gbutton" src="../../assets/btn_google_light_normal_ios.svg" alt />
+            Sing in with Google
+          </v-btn>
+        </v-card-actions>
       </v-card>
     </v-flex>
-    <v-flex xs12 sm8 md6 lg4 x3>
+    <v-flex xs12 sm6 md6 lg5 xl3 pa-3>
       <v-card>
-        <v-btn @click="login('player')">Login as Player</v-btn>
+        <v-img :src="require('@/assets/player-card.jpg')" height="300px"></v-img>
+        <v-card-title>Playar</v-card-title>
+        <v-card-subtitle>Create your characters and join chronicle</v-card-subtitle>
+        <v-card-actions>
+          <v-btn @click="login('player')" color="primary">
+            <img class="gbutton" src="../../assets/btn_google_light_normal_ios.svg" alt />
+            Sing in with Google
+          </v-btn>
+        </v-card-actions>
       </v-card>
     </v-flex>
   </v-layout>
@@ -43,4 +59,10 @@ export default {
 </script>
 
 <style>
+.gbutton {
+  width: 38px;
+  margin-left: -9px;
+  margin-top: -1px;
+  margin-right: 5px;
+}
 </style>
