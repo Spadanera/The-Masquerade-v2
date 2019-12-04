d5b2242c362aa13e933bddb7682a854f0d25254c
Nicola Zirilli <nicola.zirilli@fanplayr.com>
2019-12-04 15:01:59 +0100

    wip alert

--- client/src/views/public/Home.vue
+++ client/src/views/public/Home.vue
@@ -1,44 +1,47 @@
 <template>
-  <v-layout wrap justify-space-around v-show="loaded">
-    <v-flex xs12 sm6 md6 lg5 xl4 pa-3>
-      <v-card>
-        <v-img :src="require('@/assets/story-teller-card.png')" height="300px"></v-img>
-        <v-card-title>{{$ml.get('storyTeller')}}</v-card-title>
-        <v-card-subtitle>{{$ml.get('storyTellerLoginSubtitle')}}</v-card-subtitle>
-        <v-card-actions>
-          <v-btn @click="login('story-teller')" color="primary">
-            <img class="gbutton" src="../../assets/btn_google_light_normal_ios.svg" alt />
-            {{$ml.get('loginButton')}}
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-    <v-flex xs12 sm6 md6 lg5 xl4 pa-3>
-      <v-card>
-        <v-img :src="require('@/assets/player-card.jpg')" height="300px"></v-img>
-        <v-card-title>{{$ml.get('player')}}</v-card-title>
-        <v-card-subtitle>{{$ml.get('playerLoginSubtitle')}}</v-card-subtitle>
-        <v-card-actions>
-          <v-btn @click="login('player')" color="primary">
-            <img class="gbutton" src="../../assets/btn_google_light_normal_ios.svg" alt />
-            {{$ml.get('loginButton')}}
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-    <v-snackbar
-      v-model="snackbar.enabled"
-      :bottom="true"
-      :left="false"
-      :multi-line="false"
-      :right="false"
-      :timeout="3000"
-      :vertical="false"
-    >
-      {{ snackbar.text }}
-      <v-btn color="red" text @click="snackbar.enabled = false">Close</v-btn>
-    </v-snackbar>
-  </v-layout>
+  <div>
+    <h1>The site is under development. Do not use for real chronicles</h1>
+    <v-layout wrap justify-space-around v-show="loaded">
+      <v-flex xs12 sm6 md6 lg5 xl4 pa-3>
+        <v-card>
+          <v-img :src="require('@/assets/story-teller-card.png')" height="300px"></v-img>
+          <v-card-title>{{$ml.get('storyTeller')}}</v-card-title>
+          <v-card-subtitle>{{$ml.get('storyTellerLoginSubtitle')}}</v-card-subtitle>
+          <v-card-actions>
+            <v-btn @click="login('story-teller')" color="primary">
+              <img class="gbutton" src="../../assets/btn_google_light_normal_ios.svg" alt />
+              {{$ml.get('loginButton')}}
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 sm6 md6 lg5 xl4 pa-3>
+        <v-card>
+          <v-img :src="require('@/assets/player-card.jpg')" height="300px"></v-img>
+          <v-card-title>{{$ml.get('player')}}</v-card-title>
+          <v-card-subtitle>{{$ml.get('playerLoginSubtitle')}}</v-card-subtitle>
+          <v-card-actions>
+            <v-btn @click="login('player')" color="primary">
+              <img class="gbutton" src="../../assets/btn_google_light_normal_ios.svg" alt />
+              {{$ml.get('loginButton')}}
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-snackbar
+        v-model="snackbar.enabled"
+        :bottom="true"
+        :left="false"
+        :multi-line="false"
+        :right="false"
+        :timeout="3000"
+        :vertical="false"
+      >
+        {{ snackbar.text }}
+        <v-btn color="red" text @click="snackbar.enabled = false">Close</v-btn>
+      </v-snackbar>
+    </v-layout>
+  </div>
 </template>
 
 <script>
